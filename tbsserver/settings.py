e478919ef729b0567fe4b1ea75eb55fe18a1528f
smae101 <batistilsheila@gmail.com>
2015-10-03 07:01:23 +0800

    Added student data

--- tbsserver/settings.py
+++ tbsserver/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'tbsserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-'''
-DATABASES = {
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
-'''
+}'''
+
 DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
