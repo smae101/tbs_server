7ac80f03cc85c36d5c742dbecbe6b00e1eeefa73
Miguel Victor Remulta <migzwebhost@gmail.com>
2015-10-03 16:30:43 -0700

    Added search and filter to Student Model

--- tbs/admin.py
+++ tbs/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from tbs import models
 
+
+class StudentAdmin(admin.ModelAdmin):
+	search_fields = 'id_number', 'course', 'first_name', 'last_name'
+	list_display = 'id_number', 'first_name', 'last_name'
+	list_filter = 'course',
+
+
 admin.site.register(models.UserProfile)
-admin.site.register(models.Student)
+admin.site.register(models.Student, StudentAdmin)
 admin.site.register(models.Item)
 admin.site.register(models.Category)
 admin.site.register(models.ApprovalSellRequest)
