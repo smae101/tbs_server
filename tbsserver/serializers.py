e88c503072d85a12e332a671275f1a2674248d57
Jacquelyn Amaya <amaya.jacque@gmail.com>
2015-10-12 17:22:27 +0800

    Display "available" and "reserved" items to sell
    Exclude sold items

--- tbsserver/serializers.py
+++ tbsserver/serializers.py
@@ -182,7 +182,7 @@ class ItemsToSellViewSet(viewsets.ReadOnlyModelViewSet):
 		username = self.request.query_params.get('username', None)
 
 		if username is not None:
-			return models.Item.objects.filter(owner__user__username__iexact = username, status="Available", purpose="Sell")
+			return models.Item.objects.filter(owner__user__username__iexact = username, purpose="Sell").exclude(status="sold")
 
 		return super(ItemsToSellViewSet, self).get_queryset()
 
