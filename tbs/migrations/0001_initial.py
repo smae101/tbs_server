2c7ab506adb47a8f6b9a7c761ddc503192664c04
smae101 <batistilsheila@gmail.com>
2016-01-03 19:14:38 +0800

    new migration

--- tbs/migrations/0001_initial.py
+++ tbs/migrations/0001_initial.py
@@ -50,8 +50,8 @@ class Migration(migrations.Migration):
                 ('stars_to_use', models.IntegerField(default=0)),
                 ('picture', models.URLField()),
                 ('stars_required', models.IntegerField(default=0)),
-                ('date_approved', models.DateTimeField(verbose_name='Date Approved', blank=True, null=True)),
-                ('category', models.ForeignKey(blank=True, to='tbs.Category', null=True)),
+                ('date_approved', models.DateTimeField(verbose_name='Date Approved', null=True, blank=True)),
+                ('category', models.ForeignKey(null=True, to='tbs.Category', blank=True)),
             ],
         ),
         migrations.CreateModel(
@@ -61,12 +61,12 @@ class Migration(migrations.Migration):
                 ('item_code', models.CharField(max_length=100)),
                 ('message', models.CharField(max_length=500)),
                 ('notification_type', models.CharField(max_length=10)),
-                ('status', models.CharField(default='unread', max_length=10)),
+                ('status', models.CharField(max_length=10, default='unread')),
                 ('notification_date', models.DateTimeField(auto_now_add=True)),
                 ('notification_expiration', models.DateTimeField(default=tbs.models.Notification.expiry)),
                 ('item', models.ForeignKey(to='tbs.Item')),
-                ('maker', models.ForeignKey(default='admin', to=settings.AUTH_USER_MODEL)),
-                ('target', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='target')),
+                ('maker', models.ForeignKey(to=settings.AUTH_USER_MODEL, default='admin')),
+                ('target', models.ForeignKey(related_name='target', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
@@ -74,10 +74,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('quantity', models.IntegerField(default=0)),
-                ('item_code', models.CharField(blank=True, max_length=100, null=True)),
+                ('item_code', models.CharField(max_length=100, null=True, blank=True)),
                 ('rent_date', models.DateTimeField(auto_now_add=True)),
                 ('rent_expiration', models.DateTimeField(default=tbs.models.RentedItem.expiry)),
-                ('penalty', models.CharField(max_length=10)),
+                ('penalty', models.FloatField(default=0)),
                 ('item', models.ForeignKey(to='tbs.Item')),
                 ('renter', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
@@ -87,7 +87,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('quantity', models.IntegerField(default=0)),
-                ('item_code', models.CharField(blank=True, max_length=100, null=True)),
+                ('item_code', models.CharField(max_length=100, null=True, blank=True)),
                 ('reserved_date', models.DateTimeField(auto_now_add=True)),
                 ('request_expiration', models.DateTimeField(default=tbs.models.ReservationRequest.expiry)),
                 ('status', models.CharField(max_length=10)),
@@ -109,7 +109,7 @@ class Migration(migrations.Migration):
             name='Transaction',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
-                ('item_code', models.CharField(blank=True, max_length=100, null=True)),
+                ('item_code', models.CharField(max_length=100, null=True, blank=True)),
                 ('date_claimed', models.DateTimeField()),
             ],
             options={
@@ -121,7 +121,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('stars_collected', models.IntegerField(default=0)),
-                ('picture', models.URLField(blank=True, null=True)),
+                ('picture', models.URLField(null=True, blank=True)),
                 ('student', models.OneToOneField(to='tbs.Student')),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
@@ -129,7 +129,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='transaction',
             name='buyer',
-            field=models.ForeignKey(to='tbs.UserProfile', related_name='transactions_as_buyer'),
+            field=models.ForeignKey(related_name='transactions_as_buyer', to='tbs.UserProfile'),
         ),
         migrations.AddField(
             model_name='transaction',
@@ -139,12 +139,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='transaction',
             name='seller',
-            field=models.ForeignKey(to='tbs.UserProfile', related_name='transactions_as_owner'),
+            field=models.ForeignKey(related_name='transactions_as_owner', to='tbs.UserProfile'),
         ),
         migrations.AddField(
             model_name='item',
             name='owner',
-            field=models.ForeignKey(to='tbs.UserProfile', related_name='owner'),
+            field=models.ForeignKey(related_name='owner', to='tbs.UserProfile'),
         ),
         migrations.AddField(
             model_name='approvalsellrequest',
