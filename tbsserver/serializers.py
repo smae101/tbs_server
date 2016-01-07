ea7fa7ca74993b65b7cb3adec4390ae14d916019
Jacquelyn Amaya <amaya.jacque@gmail.com>
2016-01-08 07:19:14 +0800

    Test 11

--- tbsserver/serializers.py
+++ tbsserver/serializers.py
@@ -347,7 +347,7 @@ class AvailableItemsToSellViewSet(viewsets.ReadOnlyModelViewSet):
 		username = self.request.query_params.get('username', None)
 
 		if username is not None:
-			return models.Item.objects.filter(purpose="Sell").exclude(owner__user__username__iexact = username)
+			return models.Item.objects.filter(purpose="Sell").exclude(owner__user__username__iexact = username, status = "Pending")
 
 		return super(AvailableItemsToSellViewSet, self).get_queryset()
 
