c9b14210d3088f1191c0bb4eeed9ce3d19ec0f38
smae101 <batistilsheila@gmail.com>
2016-01-08 17:20:48 +0800

    fix na unta kaaaaa

--- tbsserver/serializers.py
+++ tbsserver/serializers.py
@@ -348,7 +348,7 @@ class AvailableItemsToSellViewSet(viewsets.ReadOnlyModelViewSet):
 		username = self.request.query_params.get('username', None)
 
 		if username is not None:
-			return models.Item.objects.filter(purpose="Sell").exclude(Q(owner__user__username__iexact = username) | Q(status="Pending" and "Disapproved") | Q(quantity=0))
+			return models.Item.objects.filter(purpose="Sell").exclude(Q(owner__user__username__iexact = username) | Q(status="Pending") | Q(status="Disapproved") | Q(quantity=0))
 
 		return super(AvailableItemsToSellViewSet, self).get_queryset()
 
@@ -361,7 +361,7 @@ class AvailableItemsForRentViewSet(viewsets.ReadOnlyModelViewSet):
 		username = self.request.query_params.get('username', None)
 
 		if username is not None:
-			return models.Item.objects.filter(purpose="Rent").exclude(Q(owner__user__username__iexact = username) | Q(status="Pending" and "Disapproved") | Q(quantity=0))
+			return models.Item.objects.filter(purpose="Rent").exclude(Q(owner__user__username__iexact = username) | Q(status="Pending") | Q(status="Disapproved") | Q(quantity=0))
 
 		return super(AvailableItemsForRentViewSet, self).get_queryset()
 
@@ -387,7 +387,7 @@ class AllDonationsViewSet(viewsets.ReadOnlyModelViewSet):
 		username = self.request.query_params.get('username', None)
 
 		if username is not None:
-			return models.Item.objects.filter(purpose="Donate").exclude(Q(owner__user__username__iexact = username) | Q(status="Pending" and "Disapproved") | Q(quantity=0))
+			return models.Item.objects.filter(purpose="Donate").exclude(Q(owner__user__username__iexact = username) | Q(status="Pending") | Q(status="Disapproved") | Q(quantity=0))
 
 		return super(AllDonationsViewSet, self).get_queryset()
 
